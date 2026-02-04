642cbbe7812c5b6c8308c1755b2b48f7fb9216e7
sufiyan <suffiyank509@gmail.com>
2026-02-05 03:24:38 +0530

    added a condition

--- Student.java
+++ Student.java
@@ -1,8 +1,9 @@
 class Student {
     public static void main(String[] args) {
         System.out.println("Java is working!");
-    }
-if(10>5){
-    system.out.println("condition: True")     
-}
-}
+    
+if (20 > 18) {
+  System.out.println("20 is greater than 18");
+System.out.println("condition: True");
+
+}}}
